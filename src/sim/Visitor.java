9c4cb9bd4c5d26faffcf38c376168995dac003fb
rockrollroger <rockrollroger@163.com>
2014-01-21 13:16:38 +0800

    Add simplified instructions

--- src/sim/Visitor.java
+++ src/sim/Visitor.java
@@ -8,4 +8,67 @@ public interface Visitor {
 	public void visit(sim.method.Method method);
 
 	public void visit(sim.annotation.Annotation.ElementLiteral elementLiteral);
+
+	public void visit(sim.stm.Instruction.Nop nop);
+
+	public void visit(sim.stm.Instruction.Move move);
+
+	public void visit(sim.stm.Instruction.MoveResult moveResult);
+
+	public void visit(sim.stm.Instruction.ReturnVoid returnVoid);
+
+	public void visit(sim.stm.Instruction.Return return1);
+
+	public void visit(sim.stm.Instruction.Const const1);
+
+	public void visit(sim.stm.Instruction.Monitor monitor);
+
+	public void visit(sim.stm.Instruction.CheckCast checkCast);
+
+	public void visit(sim.stm.Instruction.InstanceOf instanceOf);
+
+	public void visit(sim.stm.Instruction.ArrayLength arrayLength);
+
+	public void visit(sim.stm.Instruction.NewInstance newInstance);
+
+	public void visit(sim.stm.Instruction.NewArray newArray);
+
+	public void visit(sim.stm.Instruction.FilledNewArray filledNewArray);
+
+	public void visit(sim.stm.Instruction.FillArrayData fillArrayData);
+
+	public void visit(sim.stm.Instruction.Throw throw1);
+
+	public void visit(sim.stm.Instruction.Goto goto1);
+
+	public void visit(sim.stm.Instruction.Switch switch1);
+
+	public void visit(sim.stm.Instruction.Cmp cmp);
+
+	public void visit(sim.stm.Instruction.IfTest ifTest);
+
+	public void visit(sim.stm.Instruction.IfTestz ifTestz);
+
+	public void visit(sim.stm.Instruction.Aget aget);
+
+	public void visit(sim.stm.Instruction.Aput aput);
+
+	public void visit(sim.stm.Instruction.Iget iget);
+
+	public void visit(sim.stm.Instruction.Iput iput);
+
+	public void visit(sim.stm.Instruction.Sget sget);
+
+	public void visit(sim.stm.Instruction.Sput sput);
+
+	public void visit(sim.stm.Instruction.Invoke invoke);
+
+	public void visit(sim.stm.Instruction.UnOp unOp);
+
+	public void visit(sim.stm.Instruction.BinOp binOp);
+
+	public void visit(sim.stm.Instruction.BinOpTwoAddr binOpTwoAddr);
+
+	public void visit(sim.stm.Instruction.BinOpLit binOpLit);
+
 }
--- src/sim/stm/Instruction.java
+++ src/sim/stm/Instruction.java
@@ -1,4 +1,540 @@
 package sim.stm;
 
+import java.util.List;
+
+import sim.Visitor;
+
 public class Instruction {
+	public static class Nop extends T {
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Move extends T {
+		public String op;
+		public String dst;
+		public String src;
+
+		public Move(String op, String dst, String src) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.src = src;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class MoveResult extends T {
+		public String op;
+		public String dst;
+
+		public MoveResult(String op, String dst) {
+			super();
+			this.op = op;
+			this.dst = dst;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class ReturnVoid extends T {
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Return extends T {
+		public String op;
+		public String src;
+
+		public Return(String op, String src) {
+			super();
+			this.op = op;
+			this.src = src;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Const extends T {
+		public String op;
+		public String dst;
+		public String value;
+
+		public Const(String op, String dst, String value) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.value = value;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Monitor extends T {
+		public String op;
+		public String ref;
+
+		public Monitor(String op, String ref) {
+			super();
+			this.op = op;
+			this.ref = ref;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class CheckCast extends T {
+		public String ref;
+		public String type;
+
+		public CheckCast(String ref, String type) {
+			super();
+			this.ref = ref;
+			this.type = type;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class InstanceOf extends T {
+		public String dst;
+		public String ref;
+		public String type;
+
+		public InstanceOf(String dst, String ref, String type) {
+			super();
+			this.dst = dst;
+			this.ref = ref;
+			this.type = type;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class ArrayLength extends T {
+		public String dst;
+		public String ref;
+
+		public ArrayLength(String dst, String ref) {
+			super();
+			this.dst = dst;
+			this.ref = ref;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class NewInstance extends T {
+		public String dst;
+		public String type;
+
+		public NewInstance(String dst, String type) {
+			super();
+			this.dst = dst;
+			this.type = type;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class NewArray extends T {
+		public String dst;
+		public String size;
+		public String type;
+
+		public NewArray(String dst, String size, String type) {
+			super();
+			this.dst = dst;
+			this.size = size;
+			this.type = type;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class FilledNewArray extends T {
+		public String op;
+		public List<String> argList;
+		public String type;
+
+		public FilledNewArray(String op, List<String> argList, String type) {
+			super();
+			this.op = op;
+			this.argList = argList;
+			this.type = type;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class FillArrayData extends T {
+		public String ref;
+		public String label;
+
+		public FillArrayData(String ref, String label) {
+			super();
+			this.ref = ref;
+			this.label = label;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Throw extends T {
+		public String exception;
+
+		public Throw(String exception) {
+			super();
+			this.exception = exception;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Goto extends T {
+		public String op;
+		public String label;
+
+		public Goto(String op, String label) {
+			super();
+			this.op = op;
+			this.label = label;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Switch extends T {
+		public String op;
+		public String test;
+		public String label;
+
+		public Switch(String op, String test, String label) {
+			super();
+			this.op = op;
+			this.test = test;
+			this.label = label;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Cmp extends T {
+		public String op;
+		public String dst;
+		public String firstSrc;
+		public String secondSrc;
+
+		public Cmp(String op, String dst, String firstSrc, String secondSrc) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.firstSrc = firstSrc;
+			this.secondSrc = secondSrc;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class IfTest extends T {
+		public String op;
+		public String firstSrc;
+		public String secondSrc;
+		public String label;
+
+		public IfTest(String op, String firstSrc, String secondSrc, String label) {
+			super();
+			this.op = op;
+			this.firstSrc = firstSrc;
+			this.secondSrc = secondSrc;
+			this.label = label;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class IfTestz extends T {
+		public String op;
+		public String src;
+		public String label;
+
+		public IfTestz(String op, String src, String label) {
+			super();
+			this.op = op;
+			this.src = src;
+			this.label = label;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Aget extends T {
+		public String op;
+		public String dst;
+		public String array;
+		public String index;
+
+		public Aget(String op, String dst, String array, String index) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.array = array;
+			this.index = index;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Aput extends T {
+		public String op;
+		public String src;
+		public String array;
+		public String index;
+
+		public Aput(String op, String src, String array, String index) {
+			super();
+			this.op = op;
+			this.src = src;
+			this.array = array;
+			this.index = index;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Iget extends T {
+		public String op;
+		public String dst;
+		public String obj;
+		public String field;
+
+		public Iget(String op, String dst, String obj, String field) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.obj = obj;
+			this.field = field;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Iput extends T {
+		public String op;
+		public String src;
+		public String obj;
+		public String field;
+
+		public Iput(String op, String src, String obj, String field) {
+			super();
+			this.op = op;
+			this.src = src;
+			this.obj = obj;
+			this.field = field;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Sget extends T {
+		public String op;
+		public String dst;
+		public String field;
+
+		public Sget(String op, String dst, String field) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.field = field;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Sput extends T {
+		public String op;
+		public String src;
+		public String field;
+
+		public Sput(String op, String src, String field) {
+			super();
+			this.op = op;
+			this.src = src;
+			this.field = field;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class Invoke extends T {
+		public String op;
+		public String method;
+		public List<String> args;
+
+		public Invoke(String op, String method, List<String> args) {
+			super();
+			this.op = op;
+			this.method = method;
+			this.args = args;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class UnOp extends T {
+		public String op;
+		public String dst;
+		public String src;
+
+		public UnOp(String op, String dst, String src) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.src = src;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class BinOp extends T {
+		public String op;
+		public String dst;
+		public String firstSrc;
+		public String secondSrc;
+
+		public BinOp(String op, String dst, String firstSrc, String secondSrc) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.firstSrc = firstSrc;
+			this.secondSrc = secondSrc;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class BinOpTwoAddr extends T {
+		public String op;
+		public String dst;
+		public String src;
+
+		public BinOpTwoAddr(String op, String dst, String src) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.src = src;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
+	public static class BinOpLit extends T {
+		public String op;
+		public String dst;
+		public String src;
+		public String constt;
+
+		public BinOpLit(String op, String dst, String src, String constt) {
+			super();
+			this.op = op;
+			this.dst = dst;
+			this.src = src;
+			this.constt = constt;
+		}
+
+		@Override
+		public void accept(Visitor v) {
+			v.visit(this);
+		}
+	}
+
 }

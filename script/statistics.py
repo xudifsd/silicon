d26afcd21aef80633b3f76e65aef8eb550b915de
rockrollroger <rockrollroger@163.com>
2014-02-17 16:12:30 +0800

    fix some problem of statistics.py make it more accurate

--- script/statistics.py
+++ script/statistics.py
@@ -1,31 +1,34 @@
 __author__ = 'rockrollroger'
 
 import os
-import re
+#import re
 from operator import itemgetter
+
+work_dir = '/root'
 
 
 def instruction_stat(apk_list):
-    work_dir = '/root/result'
-    logfile = open('/root/result_stat/log.txt', 'a')
+    instruction_file = open('/root/carbon/docs/DalvikInsrtuctions.txt')
+    logfile = open(work_dir+'/result_stat/log.txt', 'a')
     freq_dic = {}
-    reg_instruction = re.compile(r'^[a-zA-z][\-\/|\w]+')
+#    reg_instruction = re.compile(r'^[a-zA-z][\-\/|\w]+')
+    for each_instruction in instruction_file:
+        each_instruction = each_instruction.strip('\n')
+        each_instruction = each_instruction.strip()
+        freq_dic.update({each_instruction: 0})
     for apk_name in apk_list:
         if apk_name[-3:] == 'apk':
-            smali_dir = work_dir + '/' + apk_name[:-4] + '/' + apk_name + '.smali'
+            smali_dir = work_dir+'/result' + '/' + apk_name[:-4] + '/' + apk_name + '.smali'
             try:
                 smali_file = open(smali_dir, 'r')
             except IOError:
-                logfile.writelines(apk_name+'   is missing\n')
+                logfile.writelines(apk_name+' is missing\n')
             for eachLine in smali_file:
                 try:
                     (op, rest) = eachLine.split(' ', 1)
-                    is_instruction = reg_instruction.match(op)
-                    if is_instruction:
-                        try:
-                            freq_dic[op] += 1
-                        except KeyError:
-                            freq_dic[op] = 1
+                    op = op.strip()
+                    if op in freq_dic:
+                        freq_dic[op] += 1
                 except ValueError:
                     pass
             logfile.writelines(apk_name+' is good\n')
@@ -35,12 +38,12 @@ def instruction_stat(apk_list):
 
 
 def main():
-    result_dir = '/root/result_stat'
+    result_dir = work_dir+'/result_stat'
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
-    apk_dir = '/root/apks'
+    apk_dir = work_dir+'/apks'
     catalog_list = os.walk(apk_dir).next()[1]
-    result = open('/root/result_stat/statisticsResult.txt', 'w')
+    result = open(result_dir+'/statisticsResult.txt', 'w')
     for catalog in catalog_list:
         apk_list = os.listdir(apk_dir+'/'+catalog)
         freq_list = instruction_stat(apk_list).items()

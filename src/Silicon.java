a2add6ffb0408e626a3b874dbfa60419b6035ee6
fanqiliang <fanqiliang1@126.com>
2014-04-24 09:01:14 +0800

    get the entry point

--- src/Silicon.java
+++ src/Silicon.java
@@ -5,12 +5,20 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import control.CommandLine;
 import control.Control;
 import util.MultiThreadUtils.SimplifyWorker;
 import util.MultiThreadUtils.TranslateWorker;
 import util.MultiThreadUtils.ParserWorker;
-
 import sym.SymbolicExecutor;
 
 public class Silicon {
@@ -35,6 +43,51 @@ public class Silicon {
 
 		p.waitFor();
 		return p.exitValue();
+	}
+
+	@SuppressWarnings({ "unused", "finally" })
+	public String getMain() {
+
+		String action = null;
+		String packagee = null;
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dbBuilder;
+		try {
+			dbBuilder = dbFactory.newDocumentBuilder();
+			Document doc = null;
+			doc = dbBuilder.parse("/tmp/output/AndroidManifest.xml");
+			NodeList alist = doc.getElementsByTagName("manifest");
+			OK: for (int i = 0; i < alist.getLength(); i++) {
+				Element element = (Element) alist.item(i);
+				packagee = element.getAttribute("package");
+				packagee = packagee.replace('.', '/');
+				Control.entryPoint = packagee;
+				NodeList blist = element.getElementsByTagName("application");
+				for (int j = 0; j < blist.getLength(); j++) {
+					Element belement = (Element) blist.item(j);
+					NodeList clist = belement.getElementsByTagName("activity");
+					for (int z = 0; z < clist.getLength(); z++) {
+						Element celement = (Element) clist.item(z);
+						NodeList dlist = celement.getElementsByTagName("intent-filter");
+						for (int k = 0; k < dlist.getLength(); k++) {
+							Element delement = (Element) dlist.item(i);
+							NodeList slist = delement.getElementsByTagName("action");
+							for (int s = 0; s < slist.getLength(); s++) {
+								Element selement = (Element) clist.item(s);
+								action = selement.getAttribute("android:name");
+								action = action.substring(1);
+								break OK;
+							}
+						}
+					}
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			return packagee + "/" + action;
+		}
 	}
 
 	public void run(String[] args) throws IOException, InterruptedException,
@@ -77,7 +130,7 @@ public class Silicon {
 				System.err.println("you need to specify the main Class using -entry");
 				System.exit(2);
 			}
-
+			//Control.entryPoint = getMain();
 			SymbolicExecutor symbolicExe = new SymbolicExecutor(sims, new File(
 					Control.symoutput), Control.apkoutput, Control.entryPoint);
 			symbolicExe.execute();
